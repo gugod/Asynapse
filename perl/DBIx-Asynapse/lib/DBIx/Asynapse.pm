f9c096f116a5fc9507d622636440ef067c046f84
Kang-min Liu <gugod@gugod.org>
2006-10-02 16:02:17 +0000

    r9321@GOP:  gugod | 2006-10-02 22:42:56 +0800  our $VERSION.
    git-svn-id: http://asynapse.googlecode.com/svn/trunk@4 b1d6d291-c01e-0410-9df3-bd486ced80cd

--- perl/DBIx-Asynapse/lib/DBIx/Asynapse.pm
+++ perl/DBIx-Asynapse/lib/DBIx/Asynapse.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp;
 
-use version; $VERSION = qv('0.0.2');
+use version; our $VERSION = qv('0.0.1');
 
 # Other recommended modules (uncomment to use):
 #  use IO::Prompt;
