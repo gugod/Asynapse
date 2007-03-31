ed8a262f1edad6dc2dc5fe1d7b19680c8a879d02
Kang-min Liu <gugod@gugod.org>
2007-03-31 15:48:17 +0000

    r13807@GOP:  gugod | 2007-03-31 23:47:34 +0800  Implements Jifty's REST's create_item, replace_item, delete_item functions  at JS end.
    git-svn-id: http://asynapse.googlecode.com/svn/trunk@63 b1d6d291-c01e-0410-9df3-bd486ced80cd

--- javascript/Asynapse-REST/lib/Asynapse/REST.js
+++ javascript/Asynapse-REST/lib/Asynapse/REST.js
@@ -4,10 +4,15 @@ Asynapse.REST = {}
 
 Asynapse.REST.Model = function(model) {
     this._model = model
+    this._attributes = {}
 }
 
 Asynapse.REST.Model.prototype = {
     /* ActiveRecord-like API */
+    new: function() {
+        return this;
+    },
+    
     find: function(param) {
         if ( typeof param == 'number' ) {
             return this.show_item("id", param)
@@ -18,6 +23,9 @@ Asynapse.REST.Model.prototype = {
         return this.show_item("id", id)
     },
 
+    write_attribute: function(attr, value) {
+    },
+    
     /* Jifty API */
     show_item_field: function(column, key, field) {
         var url = "/=/model/*/*/*/*.js"
@@ -59,6 +67,38 @@ Asynapse.REST.Model.prototype = {
         return this.eval_ajax_get(url);
     },
 
+    create_item: function(item) {
+        var url ="/=/model/*.js"
+            .replace("*", this._model)
+
+        new Ajax.Request(url, {
+            method: 'post',
+            asynchronous: false,
+            postBody: $H(item).toQueryString()
+        });
+    },
+    
+    replace_item: function(item) {
+        var url = "/=/action/update" + this._model + ".js"
+        new Ajax.Request(url, {
+            method: 'post',
+            contentType: 'application/x-www-form-urlencoded',
+            postBody: $H(item).toQueryString()            
+        });
+    },
+
+    delete_item: function(column, key) {
+        var url = "/=/model/*/*/*"
+            .replace("*", this._model)
+            .replace("*", column)
+            .replace("*", key)
+        
+        new Ajax.Request(url, {
+            method: 'DELETE',
+            contentType: 'application/x-www-form-urlencoded'
+        });
+    },
+    
     /* Internal Helpers */
     eval_ajax_get: function(url) {
         console.log(url)
