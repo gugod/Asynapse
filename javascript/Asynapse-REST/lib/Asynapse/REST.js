ad1cd55a5efcc7be96805f13b6a309a1b0fbbc1f
Kang-min Liu <gugod@gugod.org>
2007-03-31 14:08:12 +0000

    r13801@GOP:  gugod | 2007-03-31 22:07:46 +0800  * Add helpful helpers.  * Add type-checking to find() so it dispatch to one proper find_by_* routines.
    git-svn-id: http://asynapse.googlecode.com/svn/trunk@60 b1d6d291-c01e-0410-9df3-bd486ced80cd

--- javascript/Asynapse-REST/lib/Asynapse/REST.js
+++ javascript/Asynapse-REST/lib/Asynapse/REST.js
@@ -8,23 +8,46 @@ Asynapse.REST.Model = function(model) {
 
 Asynapse.REST.Model.prototype = {
     /* ActiveRecord-like API */
-    find: function(id) {
+    find: function(param) {
+        if ( typeof param == 'number' ) {
+            return this.show_item("id", param)
+        }
+    },
+
+    find_by_id: function(id) {
         return this.show_item("id", id)
     },
 
     /* Jifty API */
-    show_item: function(field, value) {
-        var url = "/=/model/%MODEL/%FIELD/%VALUE.js"
+    show_item: function(column, value) {
+        var url = "/=/model/%MODEL/%COLUMN/%VALUE.js"
             .replace("%MODEL", this._model)
-            .replace("%FIELD", field)
+            .replace("%COLUMN", column)
             .replace("%VALUE", value)
 
+        return this.eval_ajax_get(url);
+    },
+
+    list_model_items: function(column) {
+        var url = "/=/model/%MODEL/%COLUMN.js"
+            .replace("%MODEL", this._model)
+            .replace("%COLUMN", column)
+
+        return this.eval_ajax_get(url);
+    },
+
+    /* Internal Helpers */
+    eval_ajax_get: function(url) {
+        eval(this.ajax_get(url));
+        return $_;
+    },
+    ajax_get: function(url) {
         var req = new Ajax.Request(url, {
             method: 'GET',
             asynchronous: false
         })
-        eval( req.transport.responseText );
-        return $_;
+        return req.transport.responseText;
     }
+
 }
 
