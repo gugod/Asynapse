0acd601394ae9d8e2f1316cd90c016209f507374
Kang-min Liu <gugod@gugod.org>
2007-04-14 06:54:56 +0000

    r13914@GOP:  gugod | 2007-04-14 14:49:31 +0800  document it.
    git-svn-id: http://asynapse.googlecode.com/svn/trunk@104 b1d6d291-c01e-0410-9df3-bd486ced80cd

--- javascript/Asynapse-TextDiff/lib/Asynapse/TextDiff.js
+++ javascript/Asynapse-TextDiff/lib/Asynapse/TextDiff.js
@@ -6,6 +6,8 @@ Asynapse.TextDiff = function() {
     this.init.apply(this, arguments)
 }
 
+Asynapse.TextDiff.VERSION = "0.10";
+
 Asynapse.TextDiff.config = {
     url: "/=/action/TextDiff.json"
 }
@@ -46,4 +48,89 @@ Asynapse.TextDiff.prototype = {
         return ret;
     }
 }
+
+/**
+=head1 NAME
+
+Asynapse.TextDiff - perform diff.
+
+=head1 VERSION
+
+This document describes Asynapse.TextDiff version 0.10
+
+=head1 SYNOPSIS
+
+    var diff = new Asynapse.TextDiff( text1, text2 )
+    $("foo").innerHTML = diff.to_html();
+
+=head1 DESCRIPTION
+
+The C<Asynapse.TextDiff> class provide an easy interface to see the
+differences between two text pieces, just like the UNIX command
+C<diff>. The usage is rather simple:
+
+    var diff = new Asynapse.TextDiff( text1, text2 );
+
+The returned value is an array of array. The detail structure and
+meaning of each elements inside the array is the same as
+Algorithm::Diff::sdiff() function on CPAN.
+
+L<http://search.cpan.org/dist/Algorithm-Diff/lib/Algorithm/Diff.pm#sdiff>
+
+The actually diff algorithm is performed on the server side, not
+in javascript. You need to define a REST action that does the diff.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+Asynapse.TextDiff needs you to install a Jifty instance with REST
+plugin (which is given by default now.) And also Asynapse::TextDiff
+plugin.
+
+Since JavaScript cannot do XSS, it assumed the your Jifty instance's
+URL resides at C</>, and entry points of REST servces starts from
+C</=/>. It should be made possible to change this assumption in the
+future to match more presets in different frameworks.
+
+=head1 BUGS AND LIMITATIONS
+
+The asynapse project is hosted at L<http://code.google.com/p/asynapse/>.
+You may contact the authors or submit issues using the web interface.
+
+=head1 AUTHOR
+
+Kang-min Liu  C<< <gugod@gugod.org> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2007, Kang-min Liu C<< <gugod@gugod.org> >>. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+=cut
+*/
 
