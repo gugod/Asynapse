a3ad27e74decd0edd5a441d7d79c68ac93154a2e
Lukhnos D. Liu <lukhnos@lithoglyph.com>
2007-04-14 18:47:44 +0000

    r2406@daedalus:  lukhnos | 2007-04-02 20:53:34 +0800  Introducing the world's first Interface Builder to HTML "UI Reflector"n
    git-svn-id: http://asynapse.googlecode.com/svn/trunk@106 b1d6d291-c01e-0410-9df3-bd486ced80cd

--- cocoa/UIReflector/MyController.h
+++ cocoa/UIReflector/MyController.h
@@ -4,20 +4,7 @@
 
 @interface MyController : NSWindowController
 {
-	float flipv;
-	NSView *contentView;
-	NSMutableString *css;
-	NSMutableString *html;
-	NSMutableString *script;
-
 }
 - (IBAction)okAction:(id)sender;
 - (IBAction)worldAction:(id)sender;
-
-- (void)initReflector;
-- (void)render_html;
-- (void)render_html_obj:(id)o withBaseX:(int)base_x withBaseY:(int)base_y;
-
-- (void)add_style_for:(id)object withRect:(NSRect)f withColor:(id)color withBaseX:(int)base_x withBaseY:(int)base_y;
-
 @end
