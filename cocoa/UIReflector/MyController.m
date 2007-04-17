27f9588740b9a7e0ae5961234c91ea9048114ad5
Kang-min Liu <gugod@gugod.org>
2007-04-17 10:43:17 +0000

    r13954@GOP:  gugod | 2007-04-17 18:31:26 +0800  Revert lukhnos's revert.
    git-svn-id: http://asynapse.googlecode.com/svn/trunk@113 b1d6d291-c01e-0410-9df3-bd486ced80cd

--- cocoa/UIReflector/MyController.m
+++ cocoa/UIReflector/MyController.m
@@ -1,9 +1,5 @@
 #import "MyController.h"
 
-NSMutableString *css;
-NSMutableString *html;
-NSMutableString *script;
-
 NSString *header = 
 @"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\"><html lang=\"en\">	\
 <head>	\
@@ -15,9 +11,7 @@ NSString *header =
 </head> \
 <body>";
 
-NSString *footer = 
-@" </body></html>";
-
+NSString *footer =  @" </body></html>";
 
 int objcounter = 0;
 
@@ -31,15 +25,6 @@ NSString *funcname(SEL selector)
 	return [fn substringWithRange:NSMakeRange(0, [fn length]-1)];
 }
 
-void add_style_for_object(NSString *objname, NSRect f, float flipv, NSString *color)
-{
-	[css appendString:
-		[NSString stringWithFormat:@"#%@ { position: absolute; left:%dpx; top: %dpx; width: %dpx; height: %dpx; border: 1px solid %@; } \n",
-			objname,
-			(int)f.origin.x, (int)(flipv - (f.origin.y + f.size.height)), (int)f.size.width, (int)f.size.height, color]
-	];
-}
-
 @implementation MyController
 - (IBAction)okAction:(id)sender
 {
@@ -49,65 +34,98 @@ void add_style_for_object(NSString *objname, NSRect f, float flipv, NSString *co
 }
 
 - (void)awakeFromNib {
+	NSLog(@"awake!");
+	NSLog([[self window] description]);
+
+	[self initReflector];
+	[self render_html];
+}
+
+- (void)initReflector {
 	css  = [[NSMutableString new] retain];
 	html = [[NSMutableString new] retain];
 	script = [[NSMutableString new] retain];
-	
-	NSLog(@"awake!");
-	
-	NSLog([[self window] description]);
-	
+	contentView = [[self window] contentView];
+	flipv = [contentView frame].size.height;
+}
 
-	NSView *v = [[self window] contentView];
-	float flipv = [v frame].size.height;
-	
+#define APPEND_DIV(d)		[html appendString: [NSString stringWithFormat:@"<div id=\"%@\">\n", d]]
+#define APPEND_END_DIV(d)	[html appendString: @"</div>\n"]
+
+- (void)render_html {
 	NSString *mainframe = @"mainframe";
-	add_style_for_object(mainframe, [v frame], flipv, @"blue");	
-	
-	#define APPEND_DIV(d)		[html appendString: [NSString stringWithFormat:@"<div id=\"%@\">\n", d]]
-	#define APPEND_END_DIV(d)	[html appendString: @"</div>\n"]
-	
+	[self add_style_for:mainframe withRect:[contentView frame] withColor:@"blue" withBaseX:0 withBaseY:0];
 	APPEND_DIV(mainframe);
 	{
-		NSArray *sv = [v subviews];
-		
+		NSArray *sv = [contentView subviews];
+
 		int i;
 		for (i = 0; i<[sv count]; i++) {
-			NSString *oname = objname();
 			id o = [sv objectAtIndex:i];
-			
-			APPEND_DIV(oname);
-			
-				if ([o respondsToSelector:@selector(action)])
-				{
-					SEL s = [o action];
-					if (s) {
-						NSString *fn = funcname(s);
-						
-						[script appendString:[NSString stringWithFormat:@"function %@(e) { alert('hi'); }\n", fn]];
-						
-						[script appendString:[NSString stringWithFormat:@"Event.observe('%@', 'click', %@);\n", oname, fn]];
-					}
-				}
-			
-				if ([[o className] isEqualToString:@"NSTextField"]) {
-					[html appendString:[NSString stringWithFormat:@"%@", [o stringValue]]];
-					add_style_for_object(oname, [o frame], flipv, @"green");	
-				} else if ([[o className] isEqualToString:@"NSButton"]) {
-					[html appendString:[NSString stringWithFormat:@"%@", [o title]]];
-					add_style_for_object(oname, [o frame], flipv, @"red");					
-				}
-				else 
-				{
-					add_style_for_object(oname, [o frame], flipv, @"black");
-				}
-				
-			APPEND_END_DIV(oname);
+			[self render_html_obj:o withBaseX:0 withBaseY:0];
 		}
 	}
 	APPEND_END_DIV(mainframe);
+	NSString *str = [NSString stringWithFormat:@"%@\n\n<style>\n%@</style>\n%@\n<script>\n%@</script>\n%@\n",
+											   header, css, html, script, footer];
+	[str writeToFile:@"/tmp/nibout.html" atomically:TRUE];
+}
 
-	NSLog(@"%@\n\n<style>\n%@</style>\n%@\n<script>\n%@</script>\n%@\n", header, css, html, script, footer);
+- (void)render_html_obj:(id)o withBaseX:(int)base_x withBaseY:(int)base_y
+{
+	NSString *oname = objname();
+
+	NSLog(@"==> %@\n", [o className]);
+	
+	APPEND_DIV(oname);
+	if ([o respondsToSelector:@selector(action)])
+	{
+		SEL s = [o action];
+		if (s) {
+			NSString *fn = funcname(s);
+			
+			[script appendString:[NSString stringWithFormat:@"function %@(e) { alert('hi'); }\n", fn]];
+			[script appendString:[NSString stringWithFormat:@"Event.observe('%@', 'click', %@);\n", oname, fn]];
+		}
+	}
+
+	if ([[o className] isEqualToString:@"NSBox"]) {
+		[self add_style_for:oname withRect:[o frame] withColor:@"#aaa" withBaseX:base_x withBaseY:base_y];
+		NSRect f = [o frame];
+		NSArray *sv = [[o contentView] subviews];
+				
+		int i;
+		for (i = 0; i<[sv count]; i++) {
+			id o = [sv objectAtIndex:i];
+			[self render_html_obj:o withBaseX:f.origin.x withBaseY:f.origin.y+15];
+		}
+
+	} else if ([[o className] isEqualToString:@"NSTextField"]) {
+		[html appendString:[NSString stringWithFormat:@"%@", [o stringValue]]];
+		[self add_style_for:oname withRect:[o frame] withColor:@"green" withBaseX:base_x withBaseY:base_y];	
+	} else if ([[o className] isEqualToString:@"NSScrollView"]) {
+		[html appendString:[NSString stringWithFormat:@"<textarea>%@</textarea>", @"...."]];
+		[self add_style_for:oname withRect:[o frame] withColor:@"red"  withBaseX:base_x withBaseY:base_y];	
+					
+	} else if ([[o className] isEqualToString:@"NSButton"]) {
+		[html appendString:[NSString stringWithFormat:@"<input type=\"button\" value=\"%@\" />", [o title]]];
+		[self add_style_for:oname withRect:[o frame] withColor:@"red"  withBaseX:base_x withBaseY:base_y];	
+	} else {
+		[self add_style_for:oname withRect:[o frame] withColor:@"black"  withBaseX:base_x withBaseY:base_y];	
+	}
+	
+	APPEND_END_DIV(oname);
+}
+
+
+- (void)add_style_for:(id)objname withRect:(NSRect)f withColor:(id)color withBaseX:(int)base_x withBaseY:(int)base_y {
+	[css appendString:
+		[NSString stringWithFormat:@"#%@ { position: absolute; left:%dpx; top: %dpx; width: %dpx; height: %dpx; border: 1px solid %@; } \n",
+			objname,
+			(int)f.origin.x,
+			(int)(flipv - (f.origin.y + base_y + f.size.height)) ,
+			(int)f.size.width, (int)f.size.height, color]
+	];
 }
 
 @end
